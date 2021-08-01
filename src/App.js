9f89c231303f451d0ccc63c9b3cff74c9dd9329e
Rachelsep <rachelann.sepulveda@gmail.com>
2021-07-31 20:30:45 -0700

    Created homepage/dictionary component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
+import Homepage from "./Homepage";
 
 function App() {
-  return <div className="App">Test</div>;
+  return (
+    <div className="App">
+      <Homepage />
+      <footer>
+        <p>
+          Created by{" "}
+          <a
+            a
+            href="https://github.com/Rachelsep"
+            target="blank"
+            rel="noreferrer"
+          >
+            Rachel Sepulveda.
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Homepage.js
@@ -0,0 +1,10 @@
+export default function Homepage() {
+  return (
+    <div className="homepage">
+      <h1>Dictionary</h1>
+      <form>
+        <input type="search" placeholder="Search for a word"></input>
+      </form>
+    </div>
+  );
+}
